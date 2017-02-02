f12fe77d0a21d64941f8676a6c8e67a5463845a3
Ivan Kelber <ivankelber@gmail.com>
2017-02-02 18:23:50 -0500

    added api

--- python/MuseAPI.py
+++ python/MuseAPI.py
@@ -1,8 +1,9 @@
 import csv
 import json
 import requests
+from apiKey import apiKey
 
-access_token = "" #get this from muse api
+access_token = apiKey
 
 class MuseAPI():
     '''
@@ -12,3 +13,11 @@ class MuseAPI():
 
     def __init__(self,access_token):
         self.access_token = access_token
+
+
+
+def main():
+    mapi = MuseAPI(access_token)
+
+if __name__ == '__main__':
+    main()
