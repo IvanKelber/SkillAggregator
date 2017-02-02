2fb430cf6b535a84fdce2f57938f99af37c24e5d
Ivan Kelber <ivankelber@gmail.com>
2017-02-02 18:36:43 -0500

    first get accepted

--- python/MuseAPI.py
+++ python/MuseAPI.py
@@ -10,14 +10,26 @@ class MuseAPI():
     This class will be used to interact with the Muse API.
     Request all new job pages from API with specified fields. Return JSON/other workable format.
     '''
+    prefix_url = "https://api-v2.themuse.com/"
+
 
     def __init__(self,access_token):
         self.access_token = access_token
+
+    def get_request(self,path,params={}):
+        params['access_token'] = self.access_token
+        url = self.prefix_url + path
+        print "URL: ", url
+        # Make a request with the given url and parameters
+        # Return the JSON form of the request
+        r = requests.get(url, params=params)
+        return r.json()
 
 
 
 def main():
     mapi = MuseAPI(access_token)
+    print mapi.get_request("jobs",{"page": 1})
 
 if __name__ == '__main__':
     main()
