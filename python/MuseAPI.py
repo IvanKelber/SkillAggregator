a8de3e686d3c6262e5a9809cbc5f411a6efad898
Ivan Kelber <ivankelber@gmail.com>
2017-02-02 19:17:48 -0500

    get_all_jobs api request

--- python/MuseAPI.py
+++ python/MuseAPI.py
@@ -5,16 +5,17 @@ from apiKey import apiKey
 
 access_token = apiKey
 
-class MuseAPI():
+class MuseAPI:
     '''
     This class will be used to interact with the Muse API.
     Request all new job pages from API with specified fields. Return JSON/other workable format.
     '''
     prefix_url = "https://api-v2.themuse.com/"
-
+    default_fields = ["page","company","level","category","location"]
 
     def __init__(self,access_token):
         self.access_token = access_token
+        self.specs = {"page":0}
 
     def get_request(self,path,params={}):
         params['access_token'] = self.access_token
@@ -27,9 +28,41 @@ class MuseAPI():
 
 
 
+    def get_first_jobs_page(self):
+        self.setSpec("page",10)
+        return self.get_request("jobs",self.specs)
+
+
+    def get_all_jobs(self):
+        self.setSpec("page",0)
+        results = self.get_request("jobs",self.specs)
+        while(len(results["results"]) > 0):
+            print len(results)
+            self.setSpec("page",self.specs["page"]+1)
+            results = self.get_request("jobs",self.specs)
+
+
+
+    def setSpec(self,specification,value):
+        '''
+        specification: string
+        value: primitive/string
+        '''
+        self.specs[specification] = value
+
+    def setSpecs(self,specs):
+        '''
+        specs: dictionary (string : primitive/string)
+        '''
+        for k,v in specs.items():
+            self.specs[k] = v
+
+
+
 def main():
     mapi = MuseAPI(access_token)
-    print mapi.get_request("jobs",{"page": 1})
+    print mapi.get_all_jobs()
+    # print mapi.get_request("jobs",mapi.specs)
 
 if __name__ == '__main__':
     main()
